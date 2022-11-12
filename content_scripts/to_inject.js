5d154e85fd7579e8d57b6d64e7ce09af11554bce
Matthew Jurenka <matt.jurenka@comcast.net>
2022-11-11 23:21:26 -0800

    making injecting strategy more aggressive

--- content_scripts/to_inject.js
+++ content_scripts/to_inject.js
@@ -7,46 +7,39 @@ window.addEventListener("message", event => {
 
 let times_checked = 0
 const intervalId = setInterval(() => {
-  if (window.ethereum) {
-    window.ethereum = new Proxy(window.ethereum, {
-      get: (target, prop, receiver) => {
-        if (prop === "request") {
-          return (requestArgs) => {
-            if (requestArgs.method === "eth_sendTransaction") {
-              const { from, to, value, data } = requestArgs.params[0]
-              window.postMessage({msg_type: "simulate_transaction", from, to, value, input: data})
-              return new Promise((resolve, reject) => {
-                const check_approval_id = setInterval(() => {
-                  if (approval !== "requested") {
-                    clearInterval(check_approval_id);
-                    if (approval === "approved") {
-                      approval = "requested"
-                      resolve()
-                    } else {
-                      approval = "requested"
-                      reject({
-                        code: 4001,
-                        message: "Rejected by Core Protect"
-                      })
-                    }
-                  }
-                }, 25)
-              }).then(() => target.request(requestArgs))
-            } else {
-              return target.request(requestArgs)
+  if (window.ethereum?.request) {
+    const old_request = window.ethereum.request
+    window.ethereum.request = (requestArgs) => {
+      if (requestArgs.method === "eth_sendTransaction") {
+        const { from, to, value, data } = requestArgs.params[0]
+        window.postMessage({msg_type: "simulate_transaction", from, to, value, input: data})
+        return new Promise((resolve, reject) => {
+          const check_approval_id = setInterval(() => {
+            if (approval !== "requested") {
+              clearInterval(check_approval_id);
+              if (approval === "approved") {
+                approval = "requested"
+                resolve()
+              } else {
+                approval = "requested"
+                reject({
+                  code: 4001,
+                  message: "Rejected by Core Protect"
+                })
+              }
             }
-          }
-        } else {
-          return Reflect.get(target, prop, receiver)
-        }
+          }, 25)
+        }).then(() => old_request(requestArgs))
+      } else {
+        return old_request(requestArgs)
       }
-    })
+    }
     clearInterval(intervalId)
   } else {
     times_checked += 1
-    if (times_checked > 10) {
+    if (times_checked > 100) {
       console.warn("Core Protect could not detect MetaMask.")
       clearInterval(intervalId)
     }
   }
-}, 100)
+}, 10)
